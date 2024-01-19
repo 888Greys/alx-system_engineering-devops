1c01dab119677a31793a539e5b5150e901d6f573
888Greys <toxicgreys001@gmail.com>
2024-01-19 12:14:33 -0800

    Configuration Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
-# Installing flask
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+# Installing Flask and Werkzeug
+
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask with a specific version using pip
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug with a specific version using pip
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
